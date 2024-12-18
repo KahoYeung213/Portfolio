6e99ba403331d37235e811e406391c538594ec28
Khaos <n00220055@iadt.ie>
2024-12-18 19:58:40 +0000

    fix yep

--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -33,17 +33,17 @@ const ProjectCarousel = () => {
             </div>
             <button
                 onClick={prevSlide}
-                className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-violet-500/70 hover:bg-violet-500/90 focus:ring-4 focus:ring-violet-500 focus:outline-none p-1 rounded-full"
+                className="absolute top-1/2 left-0 transform -translate-y-1/2 bg-violet-500/70 hover:bg-violet-500/90 focus:ring-4 focus:ring-violet-500 focus:outline-none p-3 rounded-full"
             >
-                <svg className="w-4 h-4 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg className="w-8 h-8 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
                 </svg>
             </button>
             <button
                 onClick={nextSlide}
-                className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-violet-500/70 hover:bg-violet-500/90 focus:ring-4 focus:ring-violet-500 focus:outline-none p-1 rounded-full"
+                className="absolute top-1/2 right-0 transform -translate-y-1/2 bg-violet-500/70 hover:bg-violet-500/90 focus:ring-4 focus:ring-violet-500 focus:outline-none p-3 rounded-full"
             >
-                <svg className="w-4 h-4 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg className="w-8 h-8 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
                 </svg>
             </button>
